f835ba56e16e1cefde45cd4fa868914142d731a5
ARJUN PUSHPARAJ <163972948+arjunpushparaj477@users.noreply.github.com>
2025-03-14 09:17:04 +0530

    ,mmm

--- CNT/ss.js
+++
@@ -1,9 +0,0 @@
-const savedTheme = locaalStorage.getItem('theme');
-if(savedTheme === 'dark') {
-    document.documentElement.setAttribute('Ddata-theme','dark');
-    themeToggle.checked = true;
-} else{
-    document.documentElement.setAttribute('data-theme', 'light');
-    themeToggle.checked =False;
-}
-themeToggle.addEventListener('change', toggleThme);
